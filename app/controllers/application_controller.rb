0f71d7cfd10831625408ac6cd4fe70f557c793fe
DaniG2k <byakugan.87@gmail.com>
2013-10-02 10:56:58 +0100

    Moved after_sign_in_path_for method to protected

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,10 +7,6 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   
-  def after_sign_in_path_for(resource)
-    tutors_path
-  end
-  
   private
     def set_locale
       I18n.locale = params[:locale] || I18n.default_locale
@@ -19,6 +15,10 @@ class ApplicationController < ActionController::Base
     def default_url_options(options={})
       logger.debug "default_url_options is passed options: #{options.inspect}\n"
       {locale: I18n.locale} 
+    end
+    
+    def after_sign_in_path_for(resource)
+      tutors_path
     end
     
     def user_time_zone(&block)
