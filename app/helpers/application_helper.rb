36f0e2cb6ae5cacba10cd3fc8f065998e763cb69
DaniG2k <byakugan.87@gmail.com>
2013-10-10 12:50:31 +0100

    Changed tutor add field to button

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -29,7 +29,7 @@ module ApplicationHelper
     fields = f.fields_for(association, new_object, child_index: id) do |builder|
       render(association.to_s.singularize + "_fields", f: builder)
     end
-    link_to(name, '#', class: "add_fields", data: {id: id, fields: fields.gsub("\n", "")})
+    link_to(name, '#', class: "add_fields btn btn-default btn-xs", data: {id: id, fields: fields.gsub("\n", "")})
   end
   
   def distance_of_time_in_words_to_now_with_ago(from_time, include_seconds_or_options = {}) 
