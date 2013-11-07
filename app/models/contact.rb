5a8a80bcd22791c7f4497fe63e4803b2b884ba5a
DaniG2k <byakugan.87@gmail.com>
2013-11-07 16:19:07 +0000

    Added malicious field to prevent bots from spamming contact form

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -4,9 +4,10 @@ class Contact
   include ActiveModel::Conversion
   include ActiveModel::Validations
   
-  attr_accessor :name, :email, :body
+  attr_accessor :name, :email, :body, :malicious
   
   validates_presence_of :name, :email, :body
+  validates_absence_of :malicious # Used for catching spam bots
   validates :email, format: { with: /\b[A-Z0-9._%a-z\-]+@(?:[A-Z0-9a-z\-]+\.)+[A-Za-z]{2,4}\z/ }
   validates :body, length: { minimum: 10, maximum: 1000 }
   
